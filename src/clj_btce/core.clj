fbbf071133ec1fa6a4f2adf74b7b64e1763453b1
JPH <jph@hbox.ca>
2014-03-23 16:38:28 +0800

    refactored and input validation added

--- src/clj_btce/core.clj
+++ src/clj_btce/core.clj
@@ -6,129 +6,203 @@
 
 (ns clj-btce.core
   (:require [org.httpkit.client :as http]
-    [cheshire.core :refer :all]
-    [clojure.string :refer [upper-case lower-case join]]
-    [pandect.core :refer [sha512-hmac]]))
+            [cheshire.core :refer :all]
+            [clojure.string :refer [upper-case lower-case join]]
+            [clj-btce.currencies :refer :all]
+            [clj-btce.helpers :refer :all]
+            [schema.core :as s]
+            [clj-btce.validation :refer :all]
+            [pandect.core :refer [sha512-hmac]]))
 
-(def config (atom {:api-key nil 
-                   :api-secret nil 
-                   :user-agent "clj-btce 0.1.0"
-                   :trade-api-url "https://btc-e.com/tapi"
-                   :public-api-url "https://btc-e.com/api/2"
-                   :keepalive 30000
-                   :insecure false
-                   :nonce-ms 500}))
+(s/defrecord Trade
+             [date :- Long
+              price :- BigDecimal
+              amount :- BigDecimal
+              tid :- Long
+              price_currency :- [s/Str]
+              item :- [s/Str]
+              trade_type :- [s/Str]])
 
-(defn sign-params [params]
-  (if (nil? (:api-secret @config))
+(s/defrecord Ticker
+             [last :- BigDecimal
+              vol_cur :- BigDecimal
+              low :- BigDecimal
+              updated :- Long
+              buy :- BigDecimal
+              vol :- BigDecimal
+              sell :- BigDecimal
+              avg :- BigDecimal
+              server_time :- Long
+              high :- BigDecimal])
+
+(def default-http-options {:content-type "application/json"
+                           :user-agent "clj-btce 0.2.0"
+                           :insecure? false
+                           :keepalive 300})
+
+(def nonce-ms (atom 500))
+(def trade-api-url (atom "https://btc-e.com/tapi"))
+(def public-api-url (atom "https://btc-e.com/api/2"))
+
+; Utility Functions
+
+(defn sign-params [params api-secret]
+  (if (nil? api-secret)
     (throw (Exception. "api-secret not assigned"))
-     (sha512-hmac 
-    (join "&" 
-      (for [[k v] (into {} (filter second params))] 
-        (format "%s=%s" k v))) (@config :api-secret))))
+    (sha512-hmac
+     (join "&"
+           (for [[k v] (into {} (filter second params))]
+             (format "%s=%s" k v))) api-secret)))
 
-(defn nonce 
+(defn get-nonce
   "The BTC-E API requires each request to include a unique and incremented nonce integer parameter.
-  
-  I suspect this is to reduce the possibility of API users from overloading the system with 
-  parallel requests. The requirement for each new API request to have an incremented nonce 
-  is difficult to share safely across threads.
-  
+
+  The nonce is tracked per-API key. If your last request was 100 (24 hours ago), it'll expect the
+  next request to be 100+.
+
   This function provides an abbreviated time-based nonce. If you need to run parallel requests,
   then you will need to implement your own nonce-management scheme and call post-data directly.
   "
-  [] (quot (System/currentTimeMillis) (:nonce-ms @config)))
+  [] (quot (System/currentTimeMillis) @nonce-ms))
 
-; Network/Data Handling Functions
+(defn load-account [] {:api-secret (clojure.string/trim-newline (slurp "/home/user/.btce.secret"))
+                         :api-key (clojure.string/trim-newline (slurp "/home/user/.btce.key"))})
 
-(defn fetch-data 
-  "fetch-data is used by the unauthenticated, public API calls.
-  
-  You should not use this directly, as it requires a correctly formatted URL.
-  "
-  [url]
-  (let [options {:method :get 
-                 :content-type "application/json"
-                 :user-agent (:user-agent @config)
-                 :insecure? (:insecure @config) 
-                 :keepalive (:keepalive @config)}
-        response @(http/get url options)]
-    (case (:status response)
-      200 (parse-string (response :body) true)
-      (throw (Exception. response)))))
+; API Interaction
 
-(defn post-data 
-  "post-data is used for authenticated, trade api calls.
-  
-  It takes an optional map of parameters as an argument.
-  
-  You should not use post-data directly, as it requires correctly formatted parameters.
-  "
-  [& [p]]
-  (if (nil? (:api-key @config)) (throw (Exception. "api-key not assigned")))
-  
-  (let [params (filter second p) 
-        options {:method :post
-                 :content-type "application/json"
-                 :user-agent (:user-agent @config)
-                 :insecure? (:insecure @config)
-                 :headers {"Key" (:api-key @config) "Sign" (sign-params params) }
-                 :form-params params
-                 :keepalive (:keepalive @config)}
-        response @(http/post (:trade-api-url @config) options)]
-          (case (:status response)
-            200 (let [body (parse-string (response :body) true)] 
-                  (case (:success body)
-                    0 (throw (Exception. (:error body)))
-                    (:return body)))
-            (throw (Exception. response)))))
+(defn api-call [http-options & [request-type]]
+  (http/request http-options
+                (fn [{:keys [status body error] :as response}]
+                  (if error
+                    (-> "Request failed: %s"
+                        (format error)
+                        (Exception.)
+                        (throw))
 
-; Trading API - https://btc-e.com/api/documentation
+                    (case status
+                      200 (if-not (nil? request-type)
+                            (case request-type
+                              "ticker" (map->Ticker (get (parse-string body true) :ticker))
+                              "trades" (map #(map->Trade %) (parse-string body true))
+                              (parse-string body true)
+                              )
+                            (parse-string body true))
+                      (throw (Exception. response)))))))
 
-(defn get-info [] 
-  (post-data {"method" "getInfo" "nonce" (nonce)}))
+(s/defn public-api-call
+        [pair :- ValidPairs
+         endpoint :- (s/enum "ticker" "trades" "depth" "fee")
+         & [http-options]]
+        (let [url (format "%s/%s/%s" @public-api-url pair endpoint)
+              http-options (-> (merge default-http-options http-options)
+                               (assoc :method :get)
+                               (assoc :url url))]
+          (api-call http-options endpoint)))
 
-(defn get-transaction-history
-  [& {:keys [from limit from_id end_id order since end]}]
-  (post-data {"method" "TransHistory" "nonce" (nonce) "from" from "count" limit "from_id" 
-   from_id "end_id" end_id "order" order "since" since "end" end}))
+(s/defn trade-api-call
+        [account :- AccountValidation
+         params
+         & [http-options]]
 
+        (let [params (-> {:nonce (get-nonce)}
+                         (merge params)
+                         (->> (map-keys name)
+                              (filter second)))
+              headers {"Key"  (get account :api-key)
+                       "Sign" (sign-params params (get account :api-secret))}
+              http-options (-> (merge default-http-options http-options)
+                               (assoc :method :post)
+                               (assoc :url @trade-api-url)
+                               (assoc :headers headers)
+                               (assoc :form-params params))]
+          (api-call http-options)))
 
-(defn get-trade-history
-  [& {:keys [from limit from_id end_id order since end pair]}]
-  (post-data {"method" "TradeHistory" "nonce" (nonce) "from" from "count" limit "from_id" from_id
-    "end_id" end_id "order" order "since" since "end" end "pair" pair}))
+; Public API Functions
 
-(defn get-active-orders [ & [pair]] 
-  (post-data {"method" "ActiveOrders" "nonce" (nonce) "pair" pair})) 
-   
+(s/defn ^:always-validate get-ticker
+        "Get the public ticker information for the supplied pair.
 
-(defn create-trade [currency1 currency2 type rate amount]
-  (post-data {"method" "Trade" "nonce" (nonce) "pair" (format "%s_%s" (name currency1) (name currency2)) "type" type 
-      "rate" rate "amount" amount}))
+        Parameters:
+        - pair (required): Must be a valid pair (see validation.clj for list)
+        - http-options (optional): specify parameters such as keepalive, user-agent"
+        [pair :- ValidPairs
+         & [http-options :- HTTPOptionsValidation]]
+        (public-api-call pair "ticker" http-options))
 
-(defn cancel-order [orderid]
-  (post-data {"method" "CancelOrder" "order_id" orderid "nonce" (nonce)}))
+(s/defn ^:always-validate get-trades
+        "Get the public trades information for the supplied pair.
 
-; Public API - https://btc-e.com/page/2
+        Parameters:
+        - pair (required): Must be a valid pair (see validation.clj for list)
+        - http-options (optional): specify parameters such as keepalive, user-agent
 
-(defn get-fee [currency1 currency2]
-  (let [url (format "%s/%s_%s/fee" (:public-api-url @config) (name currency1) (name currency2))]
-    (fetch-data url)))
+        Trade maps are converted into Trade records for performance.
+        "
+        [pair :- ValidPairs
+         & [http-options :- HTTPOptionsValidation]]
+        (public-api-call pair "trades" http-options))
 
-(defn get-ticker [currency1 currency2]
-  (let [url (format "%s/%s_%s/ticker" (:public-api-url @config) (name currency1) (name currency2))]
-    (fetch-data url)))
+(s/defn ^:always-validate get-depth
+        "Get the public depth information for the supplied pair.
 
-(defn get-trades [currency1 currency2]
-  (let [url (format "%s/%s_%s/trades" (:public-api-url @config) (name currency1) (name currency2))]
-    (fetch-data url)))
+        Parameters:
+        - pair (required): Must be a valid pair (see validation.clj for list)
+        - http-options (optional): specify parameters such as keepalive, user-agent"
+        [pair :- ValidPairs
+         & [http-options :- HTTPOptionsValidation]]
+        (public-api-call pair "depth" http-options))
 
-(defn get-depth [currency1 currency2]
- (let [url (format "%s/%s_%s/depth" (:public-api-url @config) (name currency1) (name currency2))]
-  (fetch-data url))) 
+(s/defn ^:always-validate get-fee
+        "Get the public fee information for the supplied pair.
 
-(defn configure [& {:keys [api-key api-secret]}]
-  (if-not (nil? api-key) (swap! config assoc :api-key api-key))
-  (if-not (nil? api-secret) (swap! config assoc :api-secret api-secret)))
+        Parameters:
+        - pair (required): Must be a valid pair (see validation.clj for list)
+        - http-options (optional): specify parameters such as keepalive, user-agent"
+        [pair :- ValidPairs
+         & [http-options :- HTTPOptionsValidation]]
+        (public-api-call pair "fee" http-options))
 
+; Trading API Functions
+
+(defn get-info [account & [http-options]] (trade-api-call account {:method "getInfo"} http-options))
+
+(s/defn ^:always-validate create-order
+        "Create a buy/sell order.
+
+        Parameters:
+        - account (required): This is a map containing :api-key and :api-secret keys
+        - order (required): This is a map containing :pair, :type, :rate and :amount keys
+        - http-options (optional): Specify custom keepalive, user-agent"
+        [account :- AccountValidation
+         order :- OrderValidation
+         & [http-options :- HTTPOptionsValidation]]
+        (trade-api-call account (merge order {:method "Trade"}) http-options))
+
+(s/defn ^:always-validate cancel-order
+        "Cancel an order.
+
+        Parameters:
+        - account (required): This is a map containing :api-key and :api-secret keys
+        - order_id (required): This is the order id to cancel
+        - http-options (optional): Specify custom keepalive, user-agent"
+        [account :- AccountValidation
+         order_id :- Long
+         & [http-options :- HTTPOptionsValidation]]
+        (trade-api-call account {:order_id order_id :method "CancelOrder"} http-options))
+
+(s/defn ^:always-validate get-trade-history
+        "Retrieve completed trades history."
+        [{:keys [account history-filter http-options] :as params} :- HistoryValidation]
+        (trade-api-call account (merge history-filter {:method "TradeHistory"}) http-options))
+
+(s/defn ^:always-validate get-transaction-history
+        "Retrieve transaction history."
+        [{:keys [account history-filter http-options] :as params} :- HistoryValidation]
+        (trade-api-call account (merge history-filter {:method "TransHistory"}) http-options))
+
+(s/defn get-active-orders
+        "Retrieves open orders for currency pair."
+        [account :- AccountValidation
+         pair :- ValidPairs
+         & [http-options :- HTTPOptionsValidation]]
+        (trade-api-call account {:pair pair :method "ActiveOrders"} http-options))
