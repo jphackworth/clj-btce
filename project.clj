9335117958e5c8c9cc63433a9e56150111717034
JPH <jph@hbox.ca>
2014-03-23 16:37:35 +0800

    updated for 0.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject clj-btce "0.1.0"
+(defproject clj-btce "0.2.0-SNAPSHOT"
   :description "API support for BTC-E"
   :author {:email "jph@hackworth.be" :web "https://hackworth.be"}
   :url "https://github.com/jphackworth/clj-btce"
   :license {:name "Mozilla Public License Version 2.0"
   :url "https://www.mozilla.org/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-  [http-kit "2.1.13"]
+  [http-kit "2.1.16"]
   [pandect "0.3.0"]
-  [cheshire "5.3.1"]])
+  [cheshire "5.3.1"]
+  [prismatic/schema "0.2.1"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]]}})
