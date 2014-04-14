e9411298b7d98bbfdcc1ba66af87e038d62a746d
JPH <jph@hbox.ca>
2014-04-14 22:05:51 +0800

    updated for 0.3.0

--- project.clj
+++ project.clj
@@ -1,14 +1,17 @@
-(defproject clj-btce "0.2.1"
+(defproject clj-btce "0.3.0"
   :description "API support for BTC-E"
   :author {:email "jph@hackworth.be" :web "https://hackworth.be"}
   :url "https://github.com/jphackworth/clj-btce"
   :license {:name "Mozilla Public License Version 2.0"
   :url "https://www.mozilla.org/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-  [http-kit "2.1.16"]
-  [pandect "0.3.0"]
-  [cheshire "5.3.1"]
-  [prismatic/schema "0.2.1"]
-  [medley "0.1.5"]]
+                  [pandect "0.3.0"]
+                  [prismatic/schema "0.2.1"]
+                  [medley "0.1.5"]
+                  [aleph "0.3.2"]
+                  [clj-http "0.9.1"]
+                  [me.raynes/fs "1.4.4"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.4"]]}})
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [criterium "0.4.3"]
+]}})
